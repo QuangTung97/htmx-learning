9e921e17242fe13e8258750d6518e766e6ace3fe
tung.tq <tung.tq@teko.vn>
2024-02-06 22:11:15 +0700

    Init

---
+++ main_test.go
@@ -0,0 +1,28 @@
+package htmx
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/sebdah/goldie/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTemplate(t *testing.T) {
+	tmpl := getTemplates()
+
+	for _, subTmpl := range tmpl.Templates() {
+		fmt.Println(subTmpl.Name())
+	}
+
+	g := goldie.New(t,
+		goldie.WithFixtureDir("testdata"),
+		goldie.WithNameSuffix(".html"),
+	)
+
+	var buf bytes.Buffer
+	err := tmpl.Lookup("main.html").Execute(&buf, nil)
+	assert.Equal(t, nil, err)
+	g.Assert(t, "simple", buf.Bytes())
+}
