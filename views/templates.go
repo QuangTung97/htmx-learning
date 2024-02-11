7afa47747c7682a65e3b47423532043f067b3d50
tung.tq <tung.tq@teko.vn>
2024-02-11 19:54:32 +0700

    Refactor

---
+++ views/templates.go
@@ -0,0 +1,61 @@
+package views
+
+import (
+	"html/template"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+func findAndParseTemplates(rootDir string) (*template.Template, error) {
+	cleanRoot := filepath.Clean(rootDir)
+	prefix := len(cleanRoot) + 1
+	root := template.New("")
+
+	err := filepath.WalkDir(cleanRoot, func(path string, info os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(path, ".html") {
+			return nil
+		}
+
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		name := path[prefix:]
+		_, err = root.New(name).Parse(string(data))
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return root, nil
+}
+
+var initTemplates sync.Once
+var globalTemplate *template.Template
+
+func getTemplates() *template.Template {
+	initTemplates.Do(func() {
+		_, filename, _, ok := runtime.Caller(0)
+		if !ok {
+			panic("Can not get caller line")
+		}
+
+		templDir := path.Join(path.Dir(filename), "templ")
+		globalTemplate = template.Must(findAndParseTemplates(templDir))
+	})
+
+	return globalTemplate
+}
