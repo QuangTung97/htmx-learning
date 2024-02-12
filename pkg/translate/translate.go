11243a7a41aee0ca896c5d26830fa546587308c3
tung.tq <tung.tq@teko.vn>
2024-02-12 18:49:25 +0700

    Add Lang

--- pkg/translate/translate.go
+++ pkg/translate/translate.go
@@ -1,1 +1,44 @@
 package translate
+
+import (
+	"golang.org/x/text/language"
+)
+
+type Func func(values map[string]any) string
+
+func SimpleString(s string) Func {
+	return func(values map[string]any) string {
+		return s
+	}
+}
+
+type Text struct {
+	field     []string
+	defaultFn Func
+	lang      map[language.Base]Func
+}
+
+func (t *Text) Translate(tag language.Tag, values map[string]any) string {
+	return ""
+}
+
+type newConfig struct {
+	lang map[language.Base]Func
+}
+
+type Option func(conf *newConfig)
+
+func NewText(
+	fields []string,
+	defaultFunc Func,
+	options ...Option,
+) *Text {
+	conf := newConfig{
+		lang: map[language.Base]Func{},
+	}
+	return &Text{
+		field:     fields,
+		defaultFn: defaultFunc,
+		lang:      conf.lang,
+	}
+}
